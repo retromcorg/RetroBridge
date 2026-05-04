b69256037c334e5b3341b099d7c00a57fb4d34df
Rhys B <jetpackingwolf@gmail.com>
2026-05-04 13:25:36 +1000

    Add prefix/suffixes

--- src/main/java/org/retromc/retrobridge/bridge/permission/DummyPermissionBridge.java
+++ src/main/java/org/retromc/retrobridge/bridge/permission/DummyPermissionBridge.java
@@ -10,4 +10,20 @@ public class DummyPermissionBridge implements PermissionBridge {
     public boolean isInGroup(UUID playerUuid, String groupName) {
         return false;
     }
+
+    public String[] getGroups(UUID playerUuid) {
+        return new String[0];
+    }
+
+    public String getPrimaryGroup(UUID playerUuid) {
+        return "";
+    }
+
+    public String getPrefix(UUID playerUuid) {
+        return "";
+    }
+
+    public String getSuffix(UUID playerUuid) {
+        return "";
+    }
 }
--- src/main/java/org/retromc/retrobridge/bridge/permission/PermissionBridge.java
+++ src/main/java/org/retromc/retrobridge/bridge/permission/PermissionBridge.java
@@ -6,4 +6,12 @@ public interface PermissionBridge {
     boolean has(UUID playerUuid, String permissionNode);
 
     boolean isInGroup(UUID playerUuid, String groupName);
+
+    String[] getGroups(UUID playerUuid);
+
+    String getPrimaryGroup(UUID playerUuid);
+
+    String getPrefix(UUID playerUuid);
+
+    String getSuffix(UUID playerUuid);
 }
