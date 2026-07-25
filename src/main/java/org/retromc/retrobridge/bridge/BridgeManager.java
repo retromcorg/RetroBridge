d685f4ff259cecc12c145f2d70b2c92555f8e2b0
Rhys B <jetpackingwolf@gmail.com>
2026-07-25 17:38:52 +1000

    Version 1.0.0 - Repush due to missing import

--- src/main/java/org/retromc/retrobridge/bridge/BridgeManager.java
+++ src/main/java/org/retromc/retrobridge/bridge/BridgeManager.java
@@ -22,6 +22,7 @@ import org.retromc.retrobridge.bridge.fakequit.fundamentals.FundamentalsFakeQuit
 import org.retromc.retrobridge.bridge.permission.DummyPermissionProvider;
 import org.retromc.retrobridge.bridge.permission.PermissionBridge;
 import org.retromc.retrobridge.bridge.permission.PermissionProvider;
+import org.retromc.retrobridge.bridge.permission.jperms.JPermsPermissionProvider;
 import org.retromc.retrobridge.bridge.vanish.DummyVanishProvider;
 import org.retromc.retrobridge.bridge.vanish.VanishBridge;
 import org.retromc.retrobridge.bridge.vanish.VanishProvider;
